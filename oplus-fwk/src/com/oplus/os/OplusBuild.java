21ddccd5f57ac1d7e236f16a1dd3b7474bf688e2
Dakkshesh <dakkshesh5@gmail.com>
2023-07-22 22:23:01 +0530

    Camera: Add stripped OplusBuild class
    Only retained the sutffs relevant to us.
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

---
+++ oplus-fwk/src/com/oplus/os/OplusBuild.java
@@ -0,0 +1,34 @@
+package com.oplus.os;
+
+/* loaded from: classes4.dex */
+public class OplusBuild {
+    public static final int OplusOS_11_0 = 19;
+    public static final int OplusOS_11_1 = 20;
+    public static final int OplusOS_11_2 = 21;
+    public static final int OplusOS_1_0 = 1;
+    public static final int OplusOS_1_2 = 2;
+    public static final int OplusOS_1_4 = 3;
+    public static final int OplusOS_2_0 = 4;
+    public static final int OplusOS_2_1 = 5;
+    public static final int OplusOS_3_0 = 6;
+    public static final int OplusOS_3_1 = 7;
+    public static final int OplusOS_3_2 = 8;
+    public static final int OplusOS_5_0 = 9;
+    public static final int OplusOS_5_1 = 10;
+    public static final int OplusOS_5_2 = 11;
+    public static final int OplusOS_6_0 = 12;
+    public static final int OplusOS_6_1 = 13;
+    public static final int OplusOS_6_2 = 14;
+    public static final int OplusOS_6_7 = 15;
+    public static final int OplusOS_7_0 = 16;
+    public static final int OplusOS_7_1 = 17;
+    public static final int OplusOS_7_2 = 18;
+    /* loaded from: classes4.dex */
+    public static class VERSION {
+        public static final String RELEASE = "V11.1";;
+    }
+
+    public static int getOplusOSVERSION() {
+        return 20;
+    }
+}
